80f8b4e714ff07f4c94841e974f0ffa29f777514
Alejandro Florido KPMG <alejandrofloridoreyes@gmail.com>
2022-12-12 17:49:53 +0100

    New function view added to show, update or delete individual snippets.

--- dj_api_2/snippets/views.py
+++ dj_api_2/snippets/views.py
@@ -25,3 +25,31 @@ def snippet_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except:
+        return HttpResponse(status=404)
+    
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        # In the line below, first we tell the serializer what snippet instance we want to modify and then we pass it the new data for that instance.
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
